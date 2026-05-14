aba1b82aa65ffada5b098e4b51dc3cff2e05473d
mariam_1212 <maraimmazen86@gmail.com>
2026-05-15 01:34:03 +0300

    add error handling to ptrace GETREGS calls

--- src/registers.c
+++ src/registers.c
@@ -1,32 +1,43 @@
 #include <sys/ptrace.h>
 #include <sys/user.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <errno.h>
 #include "tracer.h"
 
 
 long get_syscall_id(pid_t child_pid) {
     struct user_regs_struct regs;
-    ptrace(PTRACE_GETREGS, child_pid, NULL, &regs);
+    if (ptrace(PTRACE_GETREGS, child_pid, NULL, &regs) == -1) {
+        perror("ptrace GETREGS failed (get_syscall_id)");
+        return -1;
+    }
     return regs.orig_rax;
 }
 
 
 void get_syscall_args(pid_t child_pid, long args[6]) {
     struct user_regs_struct regs;
-    ptrace(PTRACE_GETREGS, child_pid, NULL, &regs);
+    if (ptrace(PTRACE_GETREGS, child_pid, NULL, &regs) == -1) {
+        perror("ptrace GETREGS failed (get_syscall_args)");
+        for (int i = 0; i < 6; i++) args[i] = 0;
+        return;
+    }
 
-    args[0] = (long)regs.rdi;   
-    args[1] = (long)regs.rsi;   
-    args[2] = (long)regs.rdx;   
-    args[3] = (long)regs.r10;   
-    args[4] = (long)regs.r8;    
-    args[5] = (long)regs.r9;    
+    args[0] = (long)regs.rdi;
+    args[1] = (long)regs.rsi;
+    args[2] = (long)regs.rdx;
+    args[3] = (long)regs.r10;
+    args[4] = (long)regs.r8;
+    args[5] = (long)regs.r9;
 }
-
 
 
 long get_syscall_return(pid_t child_pid) {
     struct user_regs_struct regs;
-    ptrace(PTRACE_GETREGS, child_pid, NULL, &regs);
+    if (ptrace(PTRACE_GETREGS, child_pid, NULL, &regs) == -1) {
+        perror("ptrace GETREGS failed (get_syscall_return)");
+        return -1;
+    }
     return (long)regs.rax;
 }
